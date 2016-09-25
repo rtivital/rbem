cac88ef6dc1e0bc979a7e8d43dcc9575c3fd3d3e
rtivital <rtivital@gmail.com>
2016-09-25 15:44:08 +0300

    add basic library functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-export default function rbem() {
-  return 'hello';
+function isString(value) {
+  return typeof value === 'string';
 }
+
+export function applyModifiers(instance, modifiers, separator = '--') {
+  const selector = typeof instance === 'function' && instance.bemModule
+    ? instance()
+    : instance;
+
+  if (isString(modifiers)) {
+    return selector;
+  }
+
+  if (Array.isArray(modifiers)) {
+    let { length } = modifiers;
+    if (length === 0) { return selector; }
+
+    let result = selector;
+    while (length--) {
+      result += ` ${selector}${separator}${modifiers[length - 1]}`;
+    }
+
+    return result;
+  }
+
+  if (typeof modifiers === 'object' && modifiers !== null) {
+    const keys = Object.keys(modifiers);
+    let { length } = keys;
+
+    let result = selector;
+    while (length--) {
+      const key = keys[length - 1];
+      if (modifiers[key]) {
+        result += ` ${selector}${separator}${key}`;
+      }
+    }
+
+    return result;
+  }
+
+  return '';
+}
+
+export function block(b, separator = '__') {
+  return function element(e) {
+    if (!isString(b)) { return ''; }
+    return isString(e) ? `${b}${separator}${e}` : b;
+  };
+}
+
+Object.defineProperty(block, 'bemModule', { value: true });
